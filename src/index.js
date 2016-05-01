c60b80bebd1c9e29dcbe8c4b7f0fc442fec1aa07
Kevin Barabash <kevinb7@gmail.com>
2016-04-30 22:19:27 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { View } from 'react-native-web';
+
+ReactDOM.render(
+<h1>Hello, world!</h1>,
+    document.getElementById('example')
+);
